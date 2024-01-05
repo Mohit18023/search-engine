3a75b59bffe92c29436099dac98a194ef24dee10
mohit choudhary <mohitchoudhary1054@gmail.com>
2024-01-05 21:29:29 +0530

    database connected and frontend initialized

---
+++ frontend/src/App.tsx
@@ -0,0 +1,12 @@
+
+function App() {
+
+
+  return (
+      <div className="w-full h-screen flex justify-center items-center flex-col">
+        <h1>App</h1>
+       </div>
+  )
+}
+
+export default App
--- src/crawler.ts
+++ src/crawler.ts
@@ -1,14 +1,13 @@
 import axios from "axios";
 import { parse } from "node-html-parser";
 import { join } from "path";
-import sqlite3 from 'sqlite3';
+import sqlite3 from "sqlite3";
 
-const db = new sqlite3.Database(join(__dirname, '../database.sqlite3'));
+const db = new sqlite3.Database(join(__dirname, "..", "/database.sqlite3"));
 
 // setup the initial tables
 db.serialize(() => {
-
-    db.run(`
+  db.run(`
         CREATE TABLE IF NOT EXISTS crawl_result (
             id INTEGER PRIMARY KEY AUTOINCREMENT,
             url TEXT NOT NULL,
@@ -25,28 +24,44 @@ const crawlAndIndex = async () => {
       "https://en.wikipedia.org/wiki/Node.js"
     );
     const p = parse(http_response.data);
-    const all_href : Array<string> = p.querySelectorAll('a').map(ele =>{
-        return ele.getAttribute('href') || " ";
-    }).map((href) => {
-        if( href && href.startsWith('/') ){
-            return `https://en.wikipedia.org${href}`; 
+    const all_href: Array<string> = p
+      .querySelectorAll("a")
+      .map((ele) => {
+        return ele.getAttribute("href") || " ";
+      })
+      .map((href) => {
+        if (href && href.startsWith("/")) {
+          return `https://en.wikipedia.org${href}`;
         }
         return href;
-    });
+      });
 
-    for(let i=0; i<all_href.length; i++){
+    for (let i = 0; i < all_href.length; i++) {
+      try {
         const content = await axios.get(all_href[i]);
-        const parsed_content  = parse(content.data);
-        const title = parsed_content.querySelector('title')?.text;
-        const headings = parsed_content.querySelectorAll('h1, h2, h3, h4, h5, h6').map(ele => ele.text);
+        const parsed_content = parse(content.data);
+        const title = parsed_content.querySelector("title")?.text;
+        const headings = parsed_content
+          .querySelectorAll("h1, h2, h3, h4, h5, h6")
+          .map((ele) => ele.text);
         //const paragraphs = parsed_content.querySelectorAll('p').map(ele => ele.text);
+        const cw = db.prepare(
+          `INSERT INTO crawl_result (url, title, headings, content) VALUES (?,?,?,?)`,
+          all_href[i],
+          title,
+          headings.join("|"),
+          parsed_content.text
+        );
+        cw.finalize();
+      } catch (err) {
+        console.error(err);
+        console.error("there was some error during fetching the content");
+      }
     }
     console.log(all_href);
 
     // const all_links = all_a.filter(ele => ele.startsWith('/url?q=')).map(ele => ele.slice(7, ele.indexOf('&sa=U&ved=2')));
     // console.log(all_links);
-    
-
   } catch (error) {
     console.error(error);
   }
